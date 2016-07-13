47b86b850d577b30a14838e1c42efcb4c21af749
Hunter MacDermut <whosebluesanyway@gmail.com>
2016-07-13 16:18:49 -0600

    Pull bills list from Firebase

--- raw/script.js
+++ raw/script.js
@@ -17,20 +17,20 @@ AddBillForm
 */
 
 // Bills
-let bills = [
-  { id: 0, name: 'Rent, Arthur Properties LLC', dueDate: 1, autoPay: false, paid: true },
-  { id: 1, name: 'Health Insurance, CIGNA', dueDate: 1, autoPay: false, paid: false },
-  { id: 2, name: 'Credit Card, Barclay', dueDate: 3, autoPay: false, paid: true },
-  { id: 3, name: 'Phone, AT&T', dueDate: 4, autoPay: false, paid: true },
-  { id: 4, name: 'HBO Now', dueDate: 8, autoPay: true, paid: false },
-  { id: 5, name: 'Car Payment, Honda', dueDate: 13, autoPay: false, paid: false },
-  { id: 6, name: 'Credit Card, Capital One', dueDate: 15, autoPay: false, paid: false },
-  { id: 7, name: 'Internet, XFINITY', dueDate: 16, autoPay: false, paid: false },
-  { id: 8, name: 'Netflix', dueDate: 16, autoPay: true, paid: false },
-  { id: 9, name: 'Car Insurance, GEICO', dueDate: 23, autoPay: true, paid: false },
-  { id: 10, name: 'Renter\'s Insurance, Assurant', dueDate: 23, autoPay: true, paid: false },
-  { id: 11, name: 'Code School', dueDate: 25, autoPay: true, paid: false }
-];
+// let bills = [
+//   { id: 0, name: 'Rent, Arthur Properties LLC', dueDate: 1, autoPay: false, paid: true },
+//   { id: 1, name: 'Health Insurance, CIGNA', dueDate: 1, autoPay: false, paid: false },
+//   { id: 2, name: 'Credit Card, Barclay', dueDate: 3, autoPay: false, paid: true },
+//   { id: 3, name: 'Phone, AT&T', dueDate: 4, autoPay: false, paid: true },
+//   { id: 4, name: 'HBO Now', dueDate: 8, autoPay: true, paid: false },
+//   { id: 5, name: 'Car Payment, Honda', dueDate: 13, autoPay: false, paid: false },
+//   { id: 6, name: 'Credit Card, Capital One', dueDate: 15, autoPay: false, paid: false },
+//   { id: 7, name: 'Internet, XFINITY', dueDate: 16, autoPay: false, paid: false },
+//   { id: 8, name: 'Netflix', dueDate: 16, autoPay: true, paid: false },
+//   { id: 9, name: 'Car Insurance, GEICO', dueDate: 23, autoPay: true, paid: false },
+//   { id: 10, name: 'Renter\'s Insurance, Assurant', dueDate: 23, autoPay: true, paid: false },
+//   { id: 11, name: 'Code School', dueDate: 25, autoPay: true, paid: false }
+// ];
 
 // Utilities
 function slugify(string) {
@@ -40,25 +40,34 @@ function slugify(string) {
           .replace(/ /g, '-');
 }
 
-// console.log(slugify('Renter\'s Insurance, Assurant'));
-
 function addNewBill(autoPay, dueDate, name, paid) {
   let slugifiedName = slugify(name);
-  db.ref().push({
-    slugifiedName: {
-      autoPay,
-      dueDate,
-      paid
-    }
+  db.ref(slugifiedName).set({
+    name,
+    autoPay,
+    dueDate,
+    paid
   });
 }
 
 // Test addNewBill
-addNewBill(false, 1, 'Rent, Arthur Properties LLC', true);
+// addNewBill(false, 1, 'Rent, Arthur Properties LLC', true);
 
 // Components
 class BillsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { bills: [] };
+  }
+
+  componentWillMount() {
+    db.ref().once('value').then(snapshot => {
+      this.setState({ bills: Object.keys(snapshot.val()).map(item => snapshot.val()[item]) });
+    });
+  }
+
   render() {
+    let { bills } = this.state;
     // loop through all bills and generate a new array
     // with one of each unique date
     let dates = Array.from(new Set(bills.map(item => item.dueDate)));
@@ -68,7 +77,7 @@ class BillsList extends React.Component {
         {
           dates.map(date => {
             return (
-              <DateContainer date={ date } key={`${ date }-container`} />
+              <DateContainer date={ date } key={`${ date }-container`} bills={ bills } />
             )
           })
         }
@@ -79,6 +88,7 @@ class BillsList extends React.Component {
 
 class DateContainer extends React.Component {
   render() {
+    let { bills } = this.props;
     let calculatedDate = moment().startOf('month').add(`${ this.props.date - 1 }`, 'days').format('Do').toString();
     return (
       <div className='dateContainer'>
@@ -90,8 +100,9 @@ class DateContainer extends React.Component {
                 <Bill
                   name={ bill.name }
                   dueDate={ calculatedDate }
-                  id={ bill.id }
+                  paid={ bill.paid }
                   key={ `${ bill.name }-bill` }
+                  bills={ bills }
                 />
               );
             }
@@ -105,7 +116,7 @@ class DateContainer extends React.Component {
 class Bill extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { paid: bills[this.props.id].paid }
+    this.state = { paid: this.props.paid }
   }
 
   togglePaid() {
@@ -119,7 +130,7 @@ class Bill extends React.Component {
         <input
           type='checkbox'
           onChange={ this.togglePaid.bind(this) }
-          checked={ this.state.paid }
+          defaultChecked={ this.state.paid }
         />
       </div>
     );
